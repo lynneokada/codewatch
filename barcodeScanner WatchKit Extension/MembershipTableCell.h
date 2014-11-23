a72e27d8506a99eb5f131af59baf934839e96708
Jordan Arnesen <jordan101112@gmail.com>
2014-11-23 15:41:55 -0800

    readded class; code connections

---
+++ barcodeScanner WatchKit Extension/MembershipTableCell.h
@@ -0,0 +1,17 @@
+//
+//  MembershipTableCell.h
+//  barcodeScanner
+//
+//  Created by Jordan on 11/23/14.
+//  Copyright (c) 2014 Lynne Okada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class WKInterfaceLabel;
+
+@interface MembershipTableCell : NSObject
+
+@property (strong, nonatomic) IBOutlet WKInterfaceLabel *storeName;
+
+@end
